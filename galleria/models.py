d7d5e6305364ea683523fbec7b261403c3368058
muturi254 <petermuturi74@gmail.com>
2018-05-06 08:26:19 +0300

    added models for Image, Category, Location with a dunder function to return Query set in human language

--- galleria/models.py
+++ galleria/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.location_name
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Image(models.Model):
+    image_name = models.CharField(max_length=20)
+    image_description = models.TextField(max_length=200)
+    image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    image_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('published at')
+    image_url = models.ImageField(upload_to='images/%Y-%m-%d')
+
+    def __str__(self):
+        return self.image_name
