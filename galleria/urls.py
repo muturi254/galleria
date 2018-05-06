b9f924c16000322707729926eb28493715658b5d
muturi254 <petermuturi74@gmail.com>
2018-05-06 14:24:08 +0300

    added media url

--- galleria/urls.py
+++ galleria/urls.py
@@ -1,7 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
-# write your urls here
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
