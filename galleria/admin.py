343f9bf9fafd10cb7d76699ce3956d9946ba45c4
muturi254 <petermuturi74@gmail.com>
2018-05-06 14:23:22 +0300

    regitred models in admin

--- galleria/admin.py
+++ galleria/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Image, Location, Category
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
