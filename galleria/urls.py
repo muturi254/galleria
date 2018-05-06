559a767bd51f6dac6a71cf9f8869d04acbfcf401
muturi254 <petermuturi74@gmail.com>
2018-05-06 18:05:47 +0300

    registred the search view as a url

--- galleria/urls.py
+++ galleria/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^search/', views.search_results, name='search_results'),
 ]
 
 
