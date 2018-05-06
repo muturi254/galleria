1a9031abae1052711eab89627f654bf4b1edb20c
muturi254 <petermuturi74@gmail.com>
2018-05-06 19:09:39 +0300

    update urls

--- galleria/urls.py
+++ galleria/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/', views.search_results, name='search_results'),
+    url(r'^details/(?P<image_id>[0-9]+)/$', views.details, name='details'),
 ]
 
 
