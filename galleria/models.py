b29a090c444f8a4da7b2ff69d6a5a5fb269d5857
muturi254 <petermuturi74@gmail.com>
2018-05-06 18:06:28 +0300

    compare with lms

--- galleria/models.py
+++ galleria/models.py
@@ -39,8 +39,13 @@ class Image(models.Model):
       return cls.objects.get(pk=image_id)
 
     @classmethod
-    def search_category(cls, category_name):
-        return cls.objects.filter(image_category=category_name)
+    def search_by_title(cls, search_term):
+        news = cls.objects.filter(image_category__category_name__icontains=search_term)
+        return news
+
+    @classmethod
+    def search_category(cls, categorys_name):
+        return cls.objects.filter(image_category__category_name=categorys_name)
 
     @classmethod
     def filter_by_location(cls, location):
